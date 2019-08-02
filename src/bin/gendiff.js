d3fd150f92707d70d9814f9ee35ff21b3725a827
Ivan Nor <ivan.norik@gmail.com>
2019-08-02 12:54:34 +0300

    add commander & few logic

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -1,3 +1,26 @@
 #!/usr/bin/env node
 
-console.log('Welcome to the Gen Diff!');
+const gendiff = require('commander');
+
+// let cmdValue;
+// let envValue;
+
+gendiff
+  .version('0.1.0')
+  .option('-v, --version', 'output the version number')
+  .option('-f, --format [type]', 'output format')
+  .on('-h, --help', () => {})
+  .arguments('<firstConfig> <secondConfig>')
+  .description('Compares two configuration files and shows a difference.')
+  // .action((cmd, env) => {
+  //   cmdValue = cmd;
+  //   envValue = env;
+  // })
+  .parse(process.argv);
+
+// if (typeof cmdValue === 'undefined') {
+//   console.error('no command given!');
+//   process.exit(1);
+// }
+// console.log('command:', cmdValue);
+// console.log('environment:', envValue || 'no environment given');
