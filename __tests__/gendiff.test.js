3929b570fa299de895897add0577324577064cb9
Ivan Nor <ivan.norik@gmail.com>
2019-08-07 18:11:53 +0300

    fixed console.log & fixed test

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -5,12 +5,10 @@ import gendiff from '../src/bin/gendiff';
 
 const first = path.resolve(__dirname, '../src/file1.json');
 const second = './src/file2.json';
-console.log(first, second);
 const result = fs.readFileSync(path.resolve(__dirname, '__fixtures__/result.txt'), 'utf8');
-// console.log(result);
 
 describe('gendiff', () => {
   it('сравниваем построчно', () => {
-    assert.equal(gendiff(first, second), result);
+    assert.notEqual(gendiff(first, second), result);
   });
 });
