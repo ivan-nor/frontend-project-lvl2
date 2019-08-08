56637b4eeca5eb5b9724989d7e2cbaba45e96666
Ivan Nor <ivan.norik@gmail.com>
2019-08-08 14:09:19 +0300

    fixed

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -1,14 +1,14 @@
 import assert from 'assert';
 import fs from 'fs';
-import path from 'path';
+// import path from 'path';
 import gendiff from '../src/bin/gendiff';
 
-const first = path.resolve(__dirname, '../src/file1.json');
-const second = './src/file2.json';
-const result = fs.readFileSync(path.resolve(__dirname, '__fixtures__/result.txt'), 'utf8');
+const first = `${__dirname}/__fixtures__/before.json`;
+const second = `${__dirname}/__fixtures__/after.json`;
+const result = fs.readFileSync('__tests__/__fixtures__/result.txt', 'utf8');
 
 describe('gendiff', () => {
   it('сравниваем построчно', () => {
-    assert.notEqual(gendiff(first, second), result);
+    assert.equal(gendiff(first, second), result);
   });
 });
