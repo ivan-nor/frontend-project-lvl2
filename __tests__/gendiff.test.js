d6fd614dac9a02a8aa49d95b8bfbc4467495e156
Ivan Nor <ivan.norik@gmail.com>
2019-08-09 14:44:10 +0300

    add test.each

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -3,18 +3,37 @@ import fs from 'fs';
 // import path from 'path';
 import gendiff from '../src/bin/gendiff';
 
+const result = fs.readFileSync(`${__dirname}/__fixtures__/result.txt`, 'utf8');
+
 const firstJSON = `${__dirname}/__fixtures__/before.json`;
 const secondJSON = `${__dirname}/__fixtures__/after.json`;
-const result = fs.readFileSync(`${__dirname}/__fixtures__/result.txt`, 'utf8');
-// console.log(result);
+
 const firstYAML = `${__dirname}/__fixtures__/before.yaml`;
 const secondYAML = `${__dirname}/__fixtures__/after.yaml`;
 
-describe('gendiff', () => {
-  it('JSON', () => {
-    assert.equal(gendiff(firstJSON, secondJSON), result);
-  });
-  it('YAML', () => {
-    assert.equal(gendiff(firstYAML, secondYAML), result);
-  });
-});
+const firstINI = `${__dirname}/__fixtures__/before.ini`;
+const secondINI = `${__dirname}/__fixtures__/after.ini`;
+
+// describe('gendiff', () => {
+//   it('JSON', () => {
+//     assert.equal(gendiff(firstJSON, secondJSON), result);
+//   });
+//   it('YAML', () => {
+//     assert.equal(gendiff(firstYAML, secondYAML), result);
+//   });
+//   it('INI', () => {
+//     assert.equal(gendiff(firstINI, secondINI), result);
+//   });
+// });
+
+const arr = [
+  [firstJSON, secondJSON, result],
+  [firstYAML, secondYAML, result],
+  [firstINI, secondINI, result],
+];
+test.each(arr)(
+  '%#',
+  (a, b, expected) => {
+    expect(gendiff(a, b)).toBe(expected);
+  },
+);
