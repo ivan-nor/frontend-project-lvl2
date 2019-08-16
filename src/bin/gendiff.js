1cf8be0f42a76676ce217847106ead215b384709
Ivan Nor <ivan.norik@gmail.com>
2019-08-16 16:59:06 +0300

    fixed logic

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -3,42 +3,9 @@
 // import fs from 'fs';
 import program from 'commander';
 import path from 'path';
-import _ from 'lodash';
+// import _ from 'lodash';
 import parseFile from '../parsers';
-
-const compareFlatFiles = (first, second) => {
-  const arrToResult = (arr) => {
-    if (arr.length === 0) {
-      return '';
-    }
-    return `${arr.shift()}\n${arrToResult(arr)}`;
-  };
-
-  const compare = Object.keys(second).reduce((acc, key) => { // перебор ключей второго файла
-    if (_.has(first, key)) { // ключи совпадают, смотрим изменения
-      if (Object.values(first).includes(second[key])) { // изменений не было
-        acc.push(`    ${key}: ${second[key]}`); // добавляем строку без +/-
-      } else { // изменения были, добавляем сначала с минусом, потом с плюсом
-        acc.push(`  + ${key}: ${second[key]}`);
-        acc.push(`  - ${key}: ${first[key]}`);
-      }
-    } else { // ключа нет, просто добавляем его в результат
-      acc.push(`  + ${key}: ${second[key]}`);
-    }
-
-    return acc;
-  }, []);
-  // и проверка на удаление ключа из первого файла
-  // eslint-disable-next-line array-callback-return
-  Object.keys(first).map((key) => {
-    if (!Object.keys(second).includes(key)) {
-      compare.push(`  - ${key}: ${first[key]}`);
-    }
-  });
-
-  const result = `\n{\n${arrToResult(compare.sort())}}`;
-  return result;
-};
+import compare from '../comparer2';
 
 const genDiff = (before, after) => {
   const pathToFirst = path.resolve(__dirname, process.cwd(), before);
@@ -47,7 +14,7 @@ const genDiff = (before, after) => {
   const firstParse = parseFile(pathToFirst);
   const secondParse = parseFile(pathToSecond);
 
-  return compareFlatFiles(firstParse, secondParse);
+  return compare(firstParse, secondParse);
 };
 
 program
