6587f0676574048699cb0f5c0af946d5ec8b8a88
Ivan Nor <ivan.norik@gmail.com>
2019-08-16 15:52:41 +0300

    fixed file reading

--- src/parsers.js
+++ src/parsers.js
@@ -14,10 +14,7 @@ export default (file) => {
   const extname = path.extname(file);
 
   const pathToFile = path.resolve(__dirname, process.cwd(), file);
-  const dataOfFile = fs.readFileSync(pathToFile, 'utf8', (err, data) => {
-    if (err) throw err;
-    console.log(data);
-  });
+  const dataOfFile = fs.readFileSync(pathToFile, 'utf8');
   const parsingFile = mapping[extname](dataOfFile);
 
   return parsingFile;
