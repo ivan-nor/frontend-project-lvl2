12cdfee95e92f6dbfd1ebeb6f8e814e62db600ba
Ivan Nor <ivan.norik@gmail.com>
2019-09-24 14:09:18 +0300

    add json formatter

--- src/index.js
+++ src/index.js
@@ -3,13 +3,15 @@ import parseFile from './parsers';
 import recursiveAst from './formatters/recursive';
 import plainAst from './formatters/plain';
 import render from './renderer';
+import jsonAst from './formatters/json';
 
 const mapping = {
   'plain': item => plainAst(item),
   'recursive': item => recursiveAst(item),
+  'json': item => jsonAst(item),
 };
 
-const genDiff = (before, after, format = 'recursive') => {
+const genDiff = (before, after, format) => {
   const pathToFirst = path.resolve(__dirname, process.cwd(), before);
   const pathToSecond = path.resolve(__dirname, process.cwd(), after);
 
