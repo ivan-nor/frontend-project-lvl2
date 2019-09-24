50ea30c696d4b1cf17397aa0ae0013c3121d60fe
Ivan Nor <ivan.norik@gmail.com>
2019-09-24 15:26:25 +0300

    fixed tests

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -2,13 +2,12 @@ import assert from 'assert';
 import fs from 'fs';
 import genDiff from '../src';
 
-const newResult = fs.readFileSync(`${__dirname}/__fixtures__/plainFlatResult.txt`, 'utf8');
+const newResult = fs.readFileSync(`${__dirname}/__fixtures__/nestedResult.txt`, 'utf8');
 const firstFile = `${__dirname}/__fixtures__/nestedBefore.json`;
 const secondFile = `${__dirname}/__fixtures__/nestedAfter.json`;
-const format = 'json';
 
 describe('test', () => {
   it('yet another test', () => {
-    assert.equal(genDiff(firstFile, secondFile, format), newResult);
+    assert.equal(genDiff(firstFile, secondFile), newResult);
   });
 });
