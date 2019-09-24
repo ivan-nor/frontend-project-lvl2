a52d09915d660141dfae647867d1a112a099f002
Ivan Nor <ivan.norik@gmail.com>
2019-09-24 14:10:47 +0300

    fixed console.log's

--- src/formatters/plain.js
+++ src/formatters/plain.js
@@ -1,23 +1,13 @@
 const isObject = obj => Object.prototype.toString.call(obj) === '[object Object]';
 
 const plainAst = (ast) => {
-  // console.log('START AST', ast);
   const iter = (acc, depthName, list) => {
-    console.log('START NEW ITER ', list, '\nDEPTH NAME ', depthName);
-    console.log('\nACC ', acc);
     if (list.length === 0) {
-      console.log('END');
       return acc;
     }
     const [first, ...rest] = list;
     const { name, type, value } = first;
     const newDepthName = !depthName ? `${name}` : `${depthName}.${name}`;
-    // console.log('FIRST ', first);
-    // console.log('REST ', rest);
-    console.log('NAME ', name);
-    console.log('TYPE ', type);
-    console.log('VALUE ', value);
-    console.log('NEW DEPTHNAME ', newDepthName);
     let str;
     // если проверка на массив в VALUE дает массив c объектом, то рекурсия
     if (Array.isArray(value) && isObject(value[0]) && isObject(value[1])) {
@@ -36,11 +26,10 @@ const plainAst = (ast) => {
       str = '';
     }
     const result = `${str}`;
-    console.log('RESULT ', result);
 
     return `${iter(`${acc}${result}`, depthName, rest)}`;
   };
-  return `${iter('', '', ast)}}`;
+  return `${iter('', '', ast)}`;
 };
 
 export default plainAst;
