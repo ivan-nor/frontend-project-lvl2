2757fa8ffab4f9fecdd8473d0c55feb1b1f7909b
Ivan Nor <ivan.norik@gmail.com>
2019-09-24 14:11:42 +0300

    fixed comments

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -1,41 +1,14 @@
 import assert from 'assert';
 import fs from 'fs';
-// import path from 'path';
-// import parseFile from '../src/parsers';
 import genDiff from '../src';
-
-// const result = fs.readFileSync(`${__dirname}/__fixtures__/result.txt`, 'utf8');
-
-// const firstJSON = `${__dirname}/__fixtures__/before.json`;
-// const secondJSON = `${__dirname}/__fixtures__/after.json`;
-
-// const firstYAML = `${__dirname}/__fixtures__/before.yaml`;
-// const secondYAML = `${__dirname}/__fixtures__/after.yaml`;
-
-// const firstINI = `${__dirname}/__fixtures__/before.ini`;
-// const secondINI = `${__dirname}/__fixtures__/after.ini`;
-
-// const newFirstJSON = `${__dirname}/__fixtures__/newBefore.json`;
-// const newSecondJSON = `${__dirname}/__fixtures__/newAfter.json`;
-
-// const newFirstJSONData = fs.readFileSync(`${__dirname}/__fixtures__/flatBefore.json`, 'utf8');
-// const tempFirst = JSON.parse(newFirstJSONData);
-// const newSecondJSONData = fs.readFileSync(`${__dirname}/__fixtures__/flatAfter.json`, 'utf8');
-// const tempSecond = JSON.parse(newSecondJSONData);
 
 const newResult = fs.readFileSync(`${__dirname}/__fixtures__/plainFlatResult.txt`, 'utf8');
 const firstFile = `${__dirname}/__fixtures__/nestedBefore.json`;
 const secondFile = `${__dirname}/__fixtures__/nestedAfter.json`;
-const format = 'plain';
+const format = 'json';
 
-describe('new test 2', () => {
-  it('JSON', () => {
+describe('test', () => {
+  it('yet another test', () => {
     assert.equal(genDiff(firstFile, secondFile, format), newResult);
   });
 });
-
-// describe('test stringify', () => {
-//   it('JSON', () => {
-//     assert.equal(JSON.stringify(tempFirst), newResult);
-//   });
-// });
