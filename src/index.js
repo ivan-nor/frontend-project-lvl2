a55c2b59168b0d5e7edee2b364cd82f717794ddf
Ivan Nor <ivan.norik@gmail.com>
2019-09-24 12:57:04 +0300

    add index.js istead comparer.js

---
+++ src/index.js
@@ -0,0 +1,22 @@
+import path from 'path';
+import parseFile from './parsers';
+import recursiveAst from './formatters/recursive';
+import plainAst from './formatters/plain';
+import render from './renderer';
+
+const mapping = {
+  'plain': item => plainAst(item),
+  'recursive': item => recursiveAst(item),
+};
+
+const genDiff = (before, after, format = 'recursive') => {
+  const pathToFirst = path.resolve(__dirname, process.cwd(), before);
+  const pathToSecond = path.resolve(__dirname, process.cwd(), after);
+
+  const firstParse = parseFile(pathToFirst);
+  const secondParse = parseFile(pathToSecond);
+
+  return mapping[format](render(firstParse, secondParse));
+};
+
+export default genDiff;
