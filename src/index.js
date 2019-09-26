5fbac2597edcae1e268a037107d45ee27c19ee39
Ivan Nor <ivan.norik@gmail.com>
2019-09-26 13:33:02 +0300

    fixed naming

--- src/index.js
+++ src/index.js
@@ -6,20 +6,20 @@ import plainAst from './formatters/plain';
 import render from './renderer';
 import jsonAst from './formatters/json';
 
-const mapping = {
-  'plain': item => plainAst(item),
-  'recursive': item => recursiveAst(item),
-  'json': item => jsonAst(item),
-};
-
 const genDiff = (before, after, format = 'recursive') => {
+  const formatters = {
+    'plain': item => plainAst(item),
+    'recursive': item => recursiveAst(item),
+    'json': item => jsonAst(item),
+  };
+
   const pathToFirst = path.resolve(__dirname, process.cwd(), before);
   const pathToSecond = path.resolve(__dirname, process.cwd(), after);
 
   const firstParse = parseFile(pathToFirst);
   const secondParse = parseFile(pathToSecond);
 
-  return mapping[format](render(firstParse, secondParse));
+  return formatters[format](render(firstParse, secondParse));
 };
 
 export default genDiff;
