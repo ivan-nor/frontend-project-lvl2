5ea9bc144d8616bc70612383c942510fbaecef1d
Ivan Nor <ivan.norik@gmail.com>
2019-09-26 13:27:31 +0300

    fixed const naming

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -1,13 +1,12 @@
-import assert from 'assert';
 import fs from 'fs';
 import genDiff from '../src';
 
-const newResult = fs.readFileSync(`${__dirname}/__fixtures__/nestedResult.txt`, 'utf8');
-const firstFile = `${__dirname}/__fixtures__/nestedBefore.json`;
-const secondFile = `${__dirname}/__fixtures__/nestedAfter.json`;
+const nestedResult = fs.readFileSync(`${__dirname}/__fixtures__/nestedResult.txt`, 'utf8');
+const pathToFirst = `${__dirname}/__fixtures__/nestedBefore.json`;
+const pathToSecond = `${__dirname}/__fixtures__/nestedAfter.json`;
 
 describe('test', () => {
   it('yet another test', () => {
-    assert.equal(genDiff(firstFile, secondFile), newResult);
+    expect(genDiff(pathToFirst, pathToSecond)).toBe(nestedResult);
   });
 });
