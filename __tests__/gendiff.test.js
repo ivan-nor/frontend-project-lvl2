74c259a7fb5bc3fe9c0d6ee9c090289fcebbe710
Ivan Nor <ivan.norik@gmail.com>
2019-09-27 11:50:32 +0300

    fixed definition of constants

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -1,12 +1,11 @@
 import fs from 'fs';
 import genDiff from '../src';
 
-const nestedResult = fs.readFileSync(`${__dirname}/__fixtures__/nestedResult.txt`, 'utf8');
-const pathToFirst = `${__dirname}/__fixtures__/nestedBefore.json`;
-const pathToSecond = `${__dirname}/__fixtures__/nestedAfter.json`;
-
-describe('test', () => {
-  it('yet another test', () => {
+describe('gendiff', () => {
+  it('nested files are compared with the nestedResult.txt', () => {
+    const nestedResult = fs.readFileSync(`${__dirname}/__fixtures__/nestedResult.txt`, 'utf8');
+    const pathToFirst = `${__dirname}/__fixtures__/nestedBefore.json`;
+    const pathToSecond = `${__dirname}/__fixtures__/nestedAfter.json`;
     expect(genDiff(pathToFirst, pathToSecond)).toBe(nestedResult);
   });
 });
