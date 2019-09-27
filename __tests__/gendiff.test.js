f056e7dde5b3d0e4a1f48a5c50fe0ffd5805ff7e
Ivan Nor <ivan.norik@gmail.com>
2019-09-27 19:20:29 +0300

    delete empty line

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -6,6 +6,7 @@ describe('gendiff', () => {
     const nestedResult = fs.readFileSync(`${__dirname}/__fixtures__/nestedResult.txt`, 'utf8');
     const pathToFirst = `${__dirname}/__fixtures__/nestedBefore.json`;
     const pathToSecond = `${__dirname}/__fixtures__/nestedAfter.json`;
+
     expect(genDiff(pathToFirst, pathToSecond)).toBe(nestedResult);
   });
 });
