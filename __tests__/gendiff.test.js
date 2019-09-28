b5b9c050836aeeeefdb62eb10419923fb548d602
Ivan Nor <ivan.norik@gmail.com>
2019-09-28 13:19:31 +0300

    added few tests

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -2,11 +2,36 @@ import fs from 'fs';
 import genDiff from '../src';
 
 describe('gendiff', () => {
-  it('nested files are compared with the nestedResult.txt', () => {
+  it('nested files with nestedResult.txt, formatter - recursive', () => {
+    const formatter = 'recursive';
     const nestedResult = fs.readFileSync(`${__dirname}/__fixtures__/nestedResult.txt`, 'utf8');
     const pathToFirst = `${__dirname}/__fixtures__/nestedBefore.json`;
     const pathToSecond = `${__dirname}/__fixtures__/nestedAfter.json`;
 
-    expect(genDiff(pathToFirst, pathToSecond)).toBe(nestedResult);
+    expect(genDiff(pathToFirst, pathToSecond, formatter)).toBe(nestedResult);
+  });
+
+  it('flat files with plainFlatResult.txt, formatter - plain', () => {
+    const flatResult = fs.readFileSync(`${__dirname}/__fixtures__/plainFlatResult.txt`, 'utf8');
+    const pathToFirst = `${__dirname}/__fixtures__/flatBefore.yaml`;
+    const pathToSecond = `${__dirname}/__fixtures__/flatAfter.yaml`;
+    const formatter = 'plain';
+    expect(genDiff(pathToFirst, pathToSecond, formatter)).toBe(flatResult);
+  });
+
+  it('ini files with flatResult.txt, formatter - recursive', () => {
+    const formatter = 'recursive';
+    const pathToFirst = `${__dirname}/__fixtures__/before.ini`;
+    const pathToSecond = `${__dirname}/__fixtures__/after.ini`;
+    const flatResult = fs.readFileSync(`${__dirname}/__fixtures__/flatResult.txt`, 'utf8');
+    expect(genDiff(pathToFirst, pathToSecond, formatter)).toBe(flatResult);
+  });
+
+  it('nested yaml files with jsonNestedResult.txt, formatter - json', () => {
+    const formatter = 'json';
+    const pathToFirst = `${__dirname}/__fixtures__/nestedBefore.yaml`;
+    const pathToSecond = `${__dirname}/__fixtures__/nestedAfter.yaml`;
+    const flatResult = fs.readFileSync(`${__dirname}/__fixtures__/nestedJsonResult.txt`, 'utf8');
+    expect(genDiff(pathToFirst, pathToSecond, formatter)).toBe(flatResult);
   });
 });
