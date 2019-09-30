1a5aec422dd0fa27002b2e20d35f199068e2ecec
Ivan Nor <ivan.norik@gmail.com>
2019-09-30 21:17:47 +0300

    fixed const name

--- src/builder.js
+++ src/builder.js
@@ -1,45 +1,46 @@
 import _ from 'lodash';
 
 const buildInternalTree = (beforeData, afterData) => {
-  const allKeys = _.union(_.keys(beforeData), _.keys(afterData));
-  const result = allKeys.map((name) => {
-    if (typeof beforeData[name] === 'object' && typeof afterData[name] === 'object') {
-      return {
-        name,
-        type: 'unchanged',
-        value: buildInternalTree(beforeData[name], afterData[name])
-      };
-    }
-    if (beforeData[name] === afterData[name]) {
-      return {
-        name,
-        type: 'unchanged',
-        value: beforeData[name],
-      };
-    }
-    if (!_.has(afterData, name) && _.has(beforeData, name)) {
-      return {
-        name,
-        type: 'deleted',
-        value: beforeData[name],
-      };
-    }
-    if (!_.has(beforeData, name) && _.has(afterData, name)) {
-      return {
-        name,
-        type: 'added',
-        value: afterData[name],
-      };
-    }
-    if (beforeData[name] !== afterData[name]) {
-      return {
-        name,
-        type: 'changed',
-        value: afterData[name],
-        prevValue: beforeData[name],
-      };
-    }
-  });
+  const result = _
+    .union(_.keys(beforeData), _.keys(afterData))
+    .map((name) => {
+      if (typeof beforeData[name] === 'object' && typeof afterData[name] === 'object') {
+        return {
+          name,
+          type: 'unchanged',
+          value: buildInternalTree(beforeData[name], afterData[name])
+        };
+      }
+      if (beforeData[name] === afterData[name]) {
+        return {
+          name,
+          type: 'unchanged',
+          value: beforeData[name],
+        };
+      }
+      if (!_.has(afterData, name) && _.has(beforeData, name)) {
+        return {
+          name,
+          type: 'deleted',
+          value: beforeData[name],
+        };
+      }
+      if (!_.has(beforeData, name) && _.has(afterData, name)) {
+        return {
+          name,
+          type: 'added',
+          value: afterData[name],
+        };
+      }
+      if (beforeData[name] !== afterData[name]) {
+        return {
+          name,
+          type: 'changed',
+          value: afterData[name],
+          prevValue: beforeData[name],
+        };
+      }
+    });
   return result;
 };
 
